08a7dfc599fe1331002add0daa90cbf4770c2b86
StayJayJay <114527969+StayJayJay@users.noreply.github.com>
2026-05-11 12:59:22 +0200

    Create coefficients.py

---
+++ coefficients.py
@@ -0,0 +1,9 @@
+REGULAR_SEASON = {
+    "intercept": -1.008,
+    "home": 1.481,
+    "xg_diff": 0.104,
+    "pp_diff": 3.74,
+    "goalie": 3.67,
+    "scale_shots": 0.05,
+    "scale_pp": 0.15,
+}
