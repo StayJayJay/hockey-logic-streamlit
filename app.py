53533c3f23362f2960d4a12cdbea7051e676e627
Jakub Jirmann <jirmannjakub@gmail.com>
2026-05-11 14:00:25 +0200

    v0.0.3

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.set_page_config(
-    page_title="Hockey Logic",
+    page_title="Hockey Log.Dat",
     layout="centered"
 )
 
@@ -9,30 +9,10 @@ st.set_page_config(
 # SIDEBAR NAVIGACE
 # ======================
 
-st.title("🏒 Hockey Logic")
+st.title("Hockey Log.Dat")
 
 if st.button("📊 Predikce zápasu"):
     st.switch_page("pages/1_📊_Predikce_zapasu.py")
 
 if st.button("📂 Historie & Excel"):
     st.switch_page("pages/2_📂_Historie_a_Excel.py")
-
-# ======================
-# HLAVNÍ STRÁNKA
-# ======================
-st.title("🏒 Hockey Logic")
-
-st.markdown("""
-Tato aplikace slouží k:
-
-✅ predikci hokejových zápasů  
-✅ ukládání historie  
-✅ zadávání výsledků po zápase  
-✅ exportu do Excelu  
-
----
-
-👈 **Přejdi na jednotlivé stránky pomocí menu vlevo**
-""")
-
-st.success("Aplikace je správně nastavena ✅")
